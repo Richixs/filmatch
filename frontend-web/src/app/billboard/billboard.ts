69f09b9c0b513a72e90959ba749df25510c1fc78
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-07-28 19:38:08 -0400

    feat(frontend/billboard): add 'billboard'

---
+++ frontend-web/src/app/billboard/billboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billboard',
+  imports: [],
+  templateUrl: './billboard.html',
+  styleUrl: './billboard.scss'
+})
+export class Billboard {
+
+}
