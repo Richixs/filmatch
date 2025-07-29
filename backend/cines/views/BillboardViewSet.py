9faf72cbe1ecc36796d808b022771d4398d40847
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-07-29 03:15:21 -0400

    feat(backend/views): add get 'movies-with-cine'

--- backend/cines/views/BillboardViewSet.py
+++ backend/cines/views/BillboardViewSet.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
-from ..models import Billboard
-from ..serializers import BillboardSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from ..models import Billboard, Movie
+from ..serializers import BillboardSerializer, MovieSerializer
 
 class BillboardViewSet(viewsets.ModelViewSet):
     queryset = Billboard.objects.all()
     serializer_class = BillboardSerializer
+
+    @action(detail=False, methods=['get'], url_path='movies-with-cine')
+    def movies_with_cine(self, request):
+        movie_ids = Movie.objects.filter(billboard__isnull=False).distinct().values_list('id', flat=True)
+        return Response(list(movie_ids))
