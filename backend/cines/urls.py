9c142599d517286c5a8c4cf6f07dad88d36b82f3
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-07-28 23:21:55 -0400

    feat(backend/urls): add 'billboars' API URLs

--- backend/cines/urls.py
+++ backend/cines/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CineViewSet, MovieViewSet
+from .views import CineViewSet, MovieViewSet, BillboardViewSet
 
 router = DefaultRouter()
 router.register(r'cines', CineViewSet, basename='cine')
 router.register(r'movies', MovieViewSet, basename='movie')
+router.register(r'billboards', BillboardViewSet, basename='billboard')
 
 urlpatterns = [
     path('', include(router.urls)),
