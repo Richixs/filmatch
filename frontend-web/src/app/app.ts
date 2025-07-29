b3e96840b1e26bb6571bfb937e664eb88fe92ba7
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-07-29 01:59:31 -0400

    refactor(frontend/pages): move 'billboard' to 'pages' package

--- frontend-web/src/app/app.ts
+++ frontend-web/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
-import { Billboard } from './billboard/billboard';
+import { Header } from './components/header/header';
+import { Billboard } from './pages/billboard/billboard';
 
 @Component({
   selector: 'app-root',
