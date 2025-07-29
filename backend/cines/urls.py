c330cf1fbce3b47fa893b4ca0085c326289d4959
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-07-28 22:54:34 -0400

    feat(backend/urls): add 'movies' API URLs

--- backend/cines/urls.py
+++ backend/cines/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CineViewSet
+from .views import CineViewSet, MovieViewSet
 
 router = DefaultRouter()
 router.register(r'cines', CineViewSet, basename='cine')
+router.register(r'movies', MovieViewSet, basename='movie')
 
 urlpatterns = [
     path('', include(router.urls)),
